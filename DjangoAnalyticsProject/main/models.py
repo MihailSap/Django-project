356ee75c729f220978bc2848997c143e719b531f
mihail <sapegin2005@yandex.ru>
2025-01-14 23:57:36 +0500

    Работа с media

--- DjangoAnalyticsProject/main/models.py
+++ DjangoAnalyticsProject/main/models.py
@@ -8,6 +8,7 @@ class Relevance(models.Model):
         default='relevance'
     )
     text = models.TextField('Текст', default=None)
+    table = models.TextField('Таблица', default=None)
     graph_left = models.ImageField(
         'График №2',
         default=None,
@@ -42,13 +43,23 @@ class Skills(models.Model):
         default='skills'
     )
     graph_2015 = models.ImageField('График 2015', default=None, upload_to='skills2015')
+    table = models.TextField('Таблица', default=None)
     graph_2016 = models.ImageField('График 2016', default=None, upload_to='skills2016')
+    table2 = models.TextField('Таблица', default=None)
     graph_2017 = models.ImageField('График 2017', default=None, upload_to='skills2017')
+    table3 = models.TextField('Таблица', default=None)
     graph_2018 = models.ImageField('График 2018', default=None, upload_to='skills2018')
+    table4 = models.TextField('Таблица', default=None)
     graph_2019 = models.ImageField('График 2019', default=None, upload_to='skills2019')
+    table5 = models.TextField('Таблица', default=None)
     graph_2020 = models.ImageField('График 2020', default=None, upload_to='skills2020')
+    table6 = models.TextField('Таблица', default=None)
     graph_2021 = models.ImageField('График 2021', default=None, upload_to='skills2021')
+    table7 = models.TextField('Таблица', default=None)
     graph_2022 = models.ImageField('График 2022', default=None, upload_to='skills2022')
+    table8 = models.TextField('Таблица', default=None)
     graph_2023 = models.ImageField('График 2023', default=None, upload_to='skills2023')
+    table9 = models.TextField('Таблица', default=None)
     graph_2024 = models.ImageField('График 2024', default=None, upload_to='skills2024')
+    table10 = models.TextField('Таблица', default=None)
     text = models.TextField('Текст', default=None)
